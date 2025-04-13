2d1426644cdc369f33d011d72fe4dfabf0f311f2
wiiiii123 <hong95402@st.vimaru.edu.vn>
2025-04-13 08:52:08 +0700

    Sửa lỗi các trang

--- src/pages/QuetMaQRPage.tsx
+++ src/pages/QuetMaQRPage.tsx
@@ -100,7 +100,7 @@ const QuetMaQRPage: React.FC = () => {
               processQRData(code.data);
               setError(null); // Reset error when file upload is successful
             } else {
-              setError('Không thể đọc mã QR từ ảnh. Vui lòng thử lại với ảnh khác.');
+              setError('Không thể đọc mã QR từ ảnh. Vui lòng thử lại với ảnh khác.', code.data);
               setIsValidating(true); // Ẩn ô input khi có lỗi
             }
           }
